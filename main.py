9e337292fa4c687f2613b8df2f51182e165cb429
RaronRarenberg <43893501+RaronRarenberg@users.noreply.github.com>
2020-12-12 17:34:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,24 @@
-"""This example demonstrates a simple use of the camera. It shows a window with
-a buttoned labelled 'play' to turn the camera on and off. Note that
-not finding a camera, perhaps because gstreamer is not installed, will
-throw an exception during the kv language processing.
+'''
+Application example using build() + return
+==========================================
 
-"""
+An application can be built if you return a widget on build(), or if you set
+self.root.
+'''
 
-# Uncomment these lines to see all the messages
-# from kivy.logger import Logger
-# import logging
-# Logger.setLevel(logging.TRACE)
+import kivy
+kivy.require('1.0.7')
 
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-import time
-Builder.load_string("""
-<CameraClick>:
-    orientation: 'vertical'
-    Camera:
-        id: camera
-        resolution: (640, 480)
-        play: False
-    ToggleButton:
-        text: 'Play'
-        on_press: camera.play = not camera.play
-        size_hint_y: None
-        height: '48dp'
-    Button:
-        text: 'Capture'
-        size_hint_y: None
-        height: '48dp'
-        on_press: root.capture()
-""")
+from kivy.uix.button import Button
 
 
-class CameraClick(BoxLayout):
-    def capture(self):
-        """
-        Function to capture the images and give them the names
-        according to their captured time and date.
-        """
-        camera = self.ids['camera']
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        camera.export_to_png("IMG_{}.png".format(timestr))
-        print("Captured")
-
-
-class TestCamera(App):
+class TestApp(App):
 
     def build(self):
-        return CameraClick()
+        # return a Button() as a root widget
+        return Button(text='hello world')
 
 
-TestCamera().run()
+if __name__ == '__main__':
+    TestApp().run()
